a4ab657152f3ff80f4a9823de156c30aa09e0382
bumbledoge <dariusppopp@gmail.com>
2021-04-10 14:34:26 +0300

    new slider yooo

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
+let mousePos;
 let money = {
     shares: document.querySelector('.sharesNumber'),
     price: document.querySelector('.priceNumber'),
     holdings: document.querySelector('.holdingsNumber'),
     converter: document.querySelector('.converter')
 }
+const slider = document.querySelector('.slider');
+const axis = document.querySelector('.axis');
+const bubble = document.querySelector('.pointerValue');
 
 let sharesNumber, priceNumber, holdingsNumber;
 
@@ -11,22 +15,22 @@ let pointer = document.querySelector('.pointer');
 let pointerValue = document.querySelector('.pointerValue');
 let dobi = document.querySelector('.dobi');
 
-dobi.value = 200;
 priceInput();
+bubble.style.left = money.price.value * (680 / maxValue) + axis.offsetLeft + 'px';
 
 // INPUTS //
 
 function shareInput() {
 
-    updateinputs();
+    updateInputs();
     money.holdings.value = parseInt(holdingsNumber);
     money.converter.value = holdingsNumber * 4;
 }
 function priceInput() {
 
-    updateinputs();
+    updateInputs();
     money.holdings.value = parseInt(holdingsNumber);
-    money.converter.value = holdingsNumber * 4;
+    money.converter.value = parseInt(holdingsNumber * 4);
     pointerUpdate();
 }
 function holdingsInput() {
@@ -37,71 +41,82 @@ function holdingsInput() {
     money.price.value = priceNumber;
     pointerUpdate();
 
-    money.converter.value = holdingsNumber * 4;
+    money.converter.value = parseInt(holdingsNumber * 4);
 }
 function convertInput() {
     let convertedVal = money.converter.value;
     
-    money.holdings.value = convertedVal / 4;
+    money.holdings.value = parseInt(convertedVal / 4);
     holdingsInput();
 }
 
-function updateinputs() {
+
+function updateInputs() {
     sharesNumber = parseFloat(money.shares.value);
     priceNumber = parseFloat(money.price.value);
 
     holdingsNumber = sharesNumber * priceNumber;
 }
+
+
 function pointerUpdate() {
-    maxValue = document.querySelector('.end').value;
-    let pntrPosition = 680 / (maxValue / parseFloat(money.price.value));
-
-    pointer.style.left = pntrPosition + 'px';
-    pointerValue.style.left = pntrPosition + 'px';
-    dobi.value = parseFloat(money.price.value);
+    maxValue = document.querySelector('.maxVal').value;
+    
+    axis.max = maxValue;
+    axis.value = money.price.value;
 }
 
-dragPointer(pointer);
 
-function dragPointer(elmnt){
-    let pos1 = 0, pos2 = 0;
+axis.addEventListener('input', ()=> {
+    maxValue = document.querySelector('.maxVal').value;
+    money.price.value = axis.value;
+    priceInput();
+    bubble.style.left = money.price.value * (680 / maxValue) + axis.offsetLeft + 'px';
+    bubble.innerHTML = axis.value;
+})    
 
-    elmnt.onmousedown = dragMouseDown;
 
-    function dragMouseDown(e) {
-        e = e || window.event;
-        e.preventDefault();
+
+// dragPointer(pointer);
+// function dragPointer(elmnt){
+//     let pos1 = 0, pos2 = 0;
+
+//     elmnt.onmousedown = dragMouseDown;
+
+//     function dragMouseDown(e) {
+//         e = e || window.event;
+//         e.preventDefault();
         
-        pos2 = e.clientX;
+//         pos2 = e.clientX;
         
-        document.onmouseup = closeDragElement;
-        document.onmousemove = elementDrag;
-    }
-    function elementDrag(e) {
-        e = e || window.event;
-        e.preventDefault();
+//         document.onmouseup = closeDragElement;
+//         document.onmousemove = elementDrag;
+//     }
+//     function elementDrag(e) {
+//         e = e || window.event;
+//         e.preventDefault();
 
-        pos1 = pos2 - e.clientX;
-        pos2 = e.clientX;
+//         pos1 = pos2 - e.clientX;
+//         pos2 = e.clientX;
 
-        let newPosition = elmnt.offsetLeft - pos1;
-        if(newPosition > 0 && newPosition < 681) {
+//         let newPosition = elmnt.offsetLeft - pos1;
+//         if(newPosition > 0 && newPosition < 681) {
             
-            // THE POINTER 
-            elmnt.style.left = (elmnt.offsetLeft - pos1) + 'px';
+//             // THE POINTER 
+//             elmnt.style.left = (elmnt.offsetLeft - pos1) + 'px';
             
-            // THE VALUE CONTAINER 
-            pointerValue.style.left = (elmnt.offsetLeft - pos1) + 'px';
+//             // THE VALUE CONTAINER 
+//             pointerValue.style.left = (elmnt.offsetLeft - pos1) + 'px';
             
-            maxValue = document.querySelector('.end').value;
-            dobi.value = parseInt(newPosition * (maxValue / 680));
-            money.price.value = dobi.value;
-            priceInput();
-        }
+//             maxValue = document.querySelector('.end').value;
+//             dobi.value = parseInt(newPosition * (maxValue / 680));
+//             money.price.value = dobi.value;
+//             priceInput();
+//         }
         
-    }
-    function closeDragElement(e) {
-        document.onmouseup = null;
-        document.onmousemove = null;
-    }
-}
+//     }
+//     function closeDragElement(e) {
+//         document.onmouseup = null;
+//         document.onmousemove = null;
+//     }
+// }
